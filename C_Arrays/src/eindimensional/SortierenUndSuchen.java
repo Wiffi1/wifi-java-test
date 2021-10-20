f36035bbf050eccccea393d78c1d6aa3ed6c3a97
IrnbergerF <irnbergf@gmail.com>
2021-10-20 11:08:36 +0200

    Sortieren und Suchen - Offizelle Version

--- C_Arrays/src/eindimensional/SortierenUndSuchen.java
+++ C_Arrays/src/eindimensional/SortierenUndSuchen.java
@@ -1,7 +1,5 @@
 package eindimensional;
 
-//import inputHilfe.InputUtil;
-
 import inputHilfe.InputUtil;
 
 import java.util.Arrays;
@@ -9,36 +7,43 @@ import java.util.Random;
 
 public class SortierenUndSuchen {
     public static void main(String[] args) {
-
+        // Array-Objekt mit 12 Werten erzeugen
         int[] werte = new int[12];
+
 
         // Zufallszahlen eintragen
         // Generator für Zufallszahlen
         Random zufall = new Random();
         for (int i = 0; i < werte.length; i++) {
-            int zufallsWert = zufall.nextInt(50); // Wert zwischn 0 und 49
-            werte[i] = zufallsWert + 20 ; // Werte zwischen 20 und 69
+            int zufallsWert = zufall.nextInt(50); // Wert zwischen 0 und 49
+            werte[i] = zufallsWert + 20; // Werte zwischen 20 und 69
         }
 
-        System.out.println("Werte unsortiert");
-        for (int wert : werte){
+        System.out.println("Werte unsortiert:");
+        for (int wert : werte) {
             System.out.print(wert + " ");
         }
-        System.out.println("");
+        System.out.println();
 
         System.out.println("Welche Zahl suchen?");
         int zahl = InputUtil.readInt();
+        // Binärsuche verwenden um die Zahl zu suchen
+        // Achtung: die Suche im unsortierten Array liefert möglicherweise
+        // falsch-negative Ergebnisse
+        int index = Arrays.binarySearch(werte, zahl);
+        if (index < 0) {
+            System.out.println("Wert nicht gefunden");
+        } else {
+            System.out.printf("Wert steht am Index %d\n", index);
+        }
 
-        // Das Array sortieren Quicksort
-        // ACHTUNG, MUSS SORTIERT SEIN, DAMIT quicksearch funktioniert.
+        // das Array sortieren
         Arrays.sort(werte);
-        System.out.println("Werte sortiert: ");
+        System.out.println("Werte sortiert:");
         System.out.println(Arrays.toString(werte));
 
-        // Binärsuche verwenden, um die Zahl zu suchen.
-        int index = Arrays.binarySearch(werte, zahl);
-
         // Binärsuche im sortierten Array
+        index = Arrays.binarySearch(werte, zahl);
         if (index < 0) {
             System.out.println("Wert nicht gefunden");
         } else {
