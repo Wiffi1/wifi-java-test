2b0b164077d826b8245a252c3e341f55115cb3a6
IrnbergerF <irnbergf@gmail.com>
2021-10-27 15:51:10 +0200

    ExceptionsDemo2

--- D_Vererbung/src/exceptions2/ExceptionsDemo.java
+++ D_Vererbung/src/exceptions2/ExceptionsDemo.java
@@ -15,19 +15,27 @@ public class ExceptionsDemo {
 	private static void runCalculation() {
 		char op;
 		int number1, number2, result;
-		// Operator und Operanden einlesen
-		System.out.print("1. Operand: ");
-		number1 = Integer.parseInt(scanner.nextLine());
-		System.out.print("Welche Operation (+ - * /)? ");
-		op = scanner.nextLine().charAt(0);
-		System.out.print("2. Operand: ");
-		number2 = Integer.parseInt(scanner.nextLine());
-		// Berechnung ausführen
-		Calculator calc = new Calculator();
-		result = calc.calculate(op, number1, number2);
 
-		// Ergebnis anzeigen
-		System.out.printf("%d%c%d=%d %n", number1, op, number2, result);
+		try {
+			// Operator und Operanden einlesen
+			System.out.print("1. Operand: ");
+			number1 = Integer.parseInt(scanner.nextLine());
+			System.out.print("Welche Operation (+ - * /)? ");
+			op = scanner.nextLine().charAt(0);
+			System.out.print("2. Operand: ");
+			number2 = Integer.parseInt(scanner.nextLine());
+			// Berechnung ausführen
+			Calculator calc = new Calculator();
+			result = calc.calculate(op, number1, number2);
+
+			// Ergebnis anzeigen
+			System.out.printf("%d%c%d=%d %n", number1, op, number2, result);
+		} catch (CalculationException e) {
+			System.out.println("Fehler in der Berechnung: " + e.getMessage());
+			if (e.getCause() != null) {
+				System.out.println("\tGrund " + e.getCause().toString());
+			}
+		}
 	}
 
 }
