bea108f661c019dee7d80252001e88860707b09c
IrnbergerF <irnbergf@gmail.com>
2021-10-28 16:17:00 +0200

    Sortiere nach Gewicht Integer.compare

--- E_Generics/src/animals/sort/SortiereNachGewicht.java
+++ E_Generics/src/animals/sort/SortiereNachGewicht.java
@@ -8,7 +8,9 @@ public class SortiereNachGewicht implements Comparator<Animal> {
 
     @Override
     public int compare(Animal o1, Animal o2) {
-        int ret = o1.getWeight() - o2.getWeight();
+//        int ret = o1.getWeight() - o2.getWeight();
+        // oder:
+        int ret = Integer.compare(o1.getWeight(), o2.getWeight());
         System.out.printf("compare %d (%s) und %d (%s): %d\n",
                 o1.getWeight(), o1.getName(), o2.getWeight(), o2.getName(), ret );
         return ret;
