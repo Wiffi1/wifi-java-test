e69799277d5d996d9ffc928af9f50fb27e9f6320
IrnbergerF <irnbergf@gmail.com>
2021-10-28 16:26:44 +0200

    MyStack.java - brauche ich das?

--- E_Generics/src/genericStack/MyStack.java
+++ E_Generics/src/genericStack/MyStack.java
@@ -1,7 +1,42 @@
 package genericStack;
 
 // TODO als generische Klasse defineiren
-public class MyStack {
+// T ist der Typ der Elemente, die auf den Stapel gelegt werden
+// bei der Deklaration muss für T eine Klasse oder ein Interface angegeben werden
+public class MyStack<T> {
+    // Geht nicht in Java!
+//    private T[] array = new T[10];
+
+    // array vom unbekannen Typ kann nicht erezgut werden,
+    // daher muss man sich mit einem Object-Array behelfen
+    private Object[] array;
+
+    private int anzahl = 0;
+    private int maxAnzahl = 0;
+
+    public void push(T element) {
+        array[0] = element;
+    }
+
+    public  T pop() {
+        // TODO das Element von der obersten Position holen
+        // TODO das Element su dem Array entfernen (null setzen und anzahl verminderrn)
+        // unccol
+//        Object element = array[0];
+//        return (T) element;
+        @SuppressWarnings("unchecked")
+        T element = (T)array[0];
+        return element;
+    }
+    public  T peek() {
+        // TODO das Element von der obersten Position holen
+        return null;
+    }
+    public int size() {
+        // Anazahl der Elemente am Stapel lieferen
+        return 0;
+    }
+
     // TODO: Attribute für das Array und die Anzahl
     // TODO: die Methoden-Signatuen definieren
 
@@ -12,4 +47,9 @@ public class MyStack {
     peek: das oberste Element wird zurückgeliefert (und am Stapel belassen).
     size: die Anzahl der Elemente am Stapel wird zurückgeliefert*/
 
+    // Erzeugung des Stapels unter Angabe der Kapazität
+    public MyStack(int anzahl) {
+        array = new Object[anzahl];
+        this.maxAnzahl = 0;
+    }
 }
