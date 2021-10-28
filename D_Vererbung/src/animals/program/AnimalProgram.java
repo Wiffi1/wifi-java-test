8395962967c0a2be4d6e22cc01048fd6ce535f88
IrnbergerF <irnbergf@gmail.com>
2021-10-28 11:25:49 +0200

    filters - fertig

--- D_Vererbung/src/animals/program/AnimalProgram.java
+++ D_Vererbung/src/animals/program/AnimalProgram.java
@@ -1,6 +1,7 @@
 package animals.program;
 
 
+import animals.model.AnimalFilter;
 import animals.model.AnimalList;
 
 public class AnimalProgram {
@@ -12,8 +13,24 @@ public class AnimalProgram {
 		animals.showAll();
 		System.out.println("Vegetarier");
 		//animals.showVegetarian();
+		HerbivoreFilter filter1 = new HerbivoreFilter();
+		// Implizite Umwandlaung von HerbivoreFilter nach Animalfilter
+		// (dh. Umwandlung von der immplementierenden Klasse nach Bsisinterface)
+		animals.showAnimals(filter1);
+
 		System.out.println("Schwere Tiere");
 		//animals.heavyVegetarian();
+
+		// schwere Tiere >= 200 kg
+		// implizite Umwandlung von HeavyAnimalFilter nach Animalfilter
+		AnimalFilter filter2 = new HeavyAnimalFilter(200);
+		animals.showAnimals(filter2);
+
+		// schwere Tiere >= 500 kg
+		// implizite Umwandlung von HeavyAnimalFilter nach Animalfilter
+		System.out.println("Schwere Tiere (>= 500");
+		animals.showAnimals(new HeavyAnimalFilter(500));
+
 	}
 
 }
