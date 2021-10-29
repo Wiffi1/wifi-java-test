5ba467c39206738331e7a6282d2fda38b975d3e1
LevMan <franz.irnberger@gmail.com>
2021-10-29 07:56:37 +0200

    !!! Testvorlage.java

---
+++ B_Klassen/test/java/testvorlage/Testvorlage.java
@@ -0,0 +1,41 @@
+package testvorlage;
+
+import org.junit.jupiter.api.*;
+
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Testvorlage {
+    static final Logger log = Logger.getLogger("Logger");
+
+    @BeforeAll
+    public static void beforeClass() { /*log.info("@BeforeAll");*/ }
+
+    @AfterAll
+    public static void afterClass() { /*log.info("@AterAll");*/ }
+
+    @BeforeEach
+    public void setUp() { /*log.info("@BeforeEach");*/ }
+
+    @AfterEach
+    public void tearDown() { /*log.info("@AfterEach");*/ }
+
+    @DisplayName("simpleTest")
+    @Nested class simpleTest {
+        @Test
+        public void simpleTest1() {
+            assertTrue( true);
+        }
+    }
+
+    @DisplayName("nestedTest")
+    @Nested class nestedTest {
+        @Test
+        public void nestedTest1() {
+            assertTrue(true);
+        }
+    }
+
+}
