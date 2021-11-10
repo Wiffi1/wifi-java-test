f488ba71402e4ef7f6743cc89b97b82be2fa1ecd
IrnbergerF <irnbergf@gmail.com>
2021-11-10 10:45:33 +0100

    HelloSwing - Wifi

--- G_Swing/src/introPackage/HelloSwing.java
+++ G_Swing/src/introPackage/HelloSwing.java
@@ -6,68 +6,76 @@ import java.awt.*;
 // Klasse für unser Hauptfenster
 public class HelloSwing extends JFrame {
 
-    public HelloSwing() {
+    public HelloSwing(){
         super("Erstes Swing Programm");
-        setSize(300, 400);
+        setSize(300, 200);
 
-        // Kein automatisches Layout verwenden, da wir müssen aboluste
+        // Steuerelemente
+        // Kein automatisches Layout verwenden, dh wir müssen absolute
         // Positionen und Größen angeben
         setLayout(null);
+
         int startX = 20, startY = 20, abstand = 5;
         final int breite = 70, hoehe = 20;
 
         // Titelzeile
-        JLabel lbTitel = new JLabel("Hallo bei Swing");
-        lbTitel.setBounds(startX, startY, 225, 30);
-        lbTitel.setHorizontalAlignment(SwingConstants.CENTER);
-        // Größere Schrift
+        JLabel lblTitel = new JLabel("Hallo bei Swing");
+        lblTitel.setBounds(startX, startY, 225, 30);
+        // Textausrichtung zentriert
+        lblTitel.setHorizontalAlignment(SwingConstants.CENTER);
+        // Größere Schrift, fett und kursiv
         Font titleFont = new Font("Arial", Font.BOLD | Font.ITALIC, 20);
-        lbTitel.setFont(titleFont);
-        add(lbTitel);
+        lblTitel.setFont(titleFont);
+        add(lblTitel);
 
         // 1. Zeile: Name
         startY += hoehe * 2;
         // Beschriftungsfeld
-        JLabel lbName = new JLabel("Dein Name:");
-        lbName.setSize(breite, 20);
-        lbName.setLocation(startX, startY);
-        add(lbName);
+        JLabel lblName = new JLabel("Dein Name:");
+        // Größe des Labels
+        lblName.setSize(breite, hoehe);
+        // Position des Labels
+        lblName.setLocation(startX, startY);
+        add(lblName);
 
+        // Eingabefeld für Text
         JTextField txtName = new JTextField();
-        txtName.setBounds(startX + abstand + breite, startY, 100, hoehe);
+        // Größe und Position in einem
+        txtName.setBounds(startX + abstand + breite, startY, 150, hoehe);
         add(txtName);
 
         // 2. Zeile: Button
+        startY += hoehe + abstand;
+        // Schaltfläche
         JButton btnGruesse = new JButton("Klick mich!");
-        startY += + abstand + hoehe;
-        btnGruesse.setBounds(startX + abstand + breite, startY, 100, 20);
+        btnGruesse.setBounds(startX + abstand + breite, startY, 100, hoehe);
 
         add(btnGruesse);
 
-        // 3. Feld für Message
-        startY += + abstand + hoehe;
-        JLabel lbMessage = new JLabel("");
-        lbMessage.setSize(breite, 20);
-        lbMessage.setLocation(startX, startY);
-        lbMessage.setBounds(startX, startY, 200, hoehe);
-        lbMessage.setBorder(BorderFactory.createLineBorder(Color.ORANGE));
-        add(lbMessage);
+        // 3. Zeile: Feld für Message
+        startY += hoehe + abstand;
+        JLabel lblMessage = new JLabel("");
+        lblMessage.setBounds(startX, startY, 225, hoehe);
+        lblMessage.setBorder(BorderFactory.createLineBorder(Color.ORANGE));
+        add(lblMessage);
 
-        // Callback für das Klick-Ereignis
+        // Callback für das Click-Ereignis registrieren
+        // (Lambda expression als Handler)
         btnGruesse.addActionListener(e -> {
-            System.out.println("Click auf btngrüße. name = " + txtName.getText());
-            lbMessage.setText("Hallo, " + txtName.getText());
+            System.out.println("Click auf btnGruesse, name = " + txtName.getText());
+            lblMessage.setText("Hallo, " + txtName.getText());
         });
-
-        // Standard-Operation  ist Verbergen
-        // stattdessen das Fenster beim Schließen zerstöre
-        // damit wird das Programm bbeendet, wenn dieses Fenster das letzte
+        // Standard-Operation ist Verbergen
+        // stattdessen: das Fenster beim Schließen zerstören
+        // damit wird das Programm beendet, wenn dieses Fenster das letzte
         // Fenster der Anwendung ist
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
     }
 
     public static void main(String[] args) {
         HelloSwing hauptfenster = new HelloSwing();
+        // das Hauptfenster anzeigen
         hauptfenster.setVisible(true);
     }
 
--- G_Swing/src/introPackage/HelloSwing2.java
+++ G_Swing/src/introPackage/HelloSwing2.java
@@ -9,84 +9,83 @@ import java.awt.event.ActionListener;
 public class HelloSwing2 extends JFrame implements ActionListener {
 
     // Attribute für alle Controls, die wir in den Callbacks usw. brauchen
-
     final JTextField txtName;
-    final JLabel lbMessage;
+    final JLabel lblMessage;
     final JButton btnGruesse;
 
-    public HelloSwing2() {
+    public HelloSwing2(){
         super("Erstes Swing Programm");
-        setSize(300, 400);
+        setSize(300, 200);
 
-        // Kein automatisches Layout verwenden, da wir müssen aboluste
+        // Steuerelemente
+        // Kein automatisches Layout verwenden, dh wir müssen absolute
         // Positionen und Größen angeben
         setLayout(null);
+
         int startX = 20, startY = 20, abstand = 5;
         final int breite = 70, hoehe = 20;
 
         // Titelzeile
-        JLabel lbTitel = new JLabel("Hallo bei Swing");
-        lbTitel.setBounds(startX, startY, 225, 30);
-        lbTitel.setHorizontalAlignment(SwingConstants.CENTER);
-        // Größere Schrift
+        JLabel lblTitel = new JLabel("Hallo bei Swing (2)");
+        lblTitel.setBounds(startX, startY, 225, 30);
+        // Textausrichtung zentriert
+        lblTitel.setHorizontalAlignment(SwingConstants.CENTER);
+        // Größere Schrift, fett und kursiv
         Font titleFont = new Font("Arial", Font.BOLD | Font.ITALIC, 20);
-        lbTitel.setFont(titleFont);
-        add(lbTitel);
+        lblTitel.setFont(titleFont);
+        add(lblTitel);
 
         // 1. Zeile: Name
         startY += hoehe * 2;
         // Beschriftungsfeld
-        JLabel lbName = new JLabel("Dein Name:");
-        lbName.setSize(breite, 20);
-        lbName.setLocation(startX, startY);
-        add(lbName);
+        JLabel lblName = new JLabel("Dein Name:");
+        // Größe des Labels
+        lblName.setSize(breite, hoehe);
+        // Position des Labels
+        lblName.setLocation(startX, startY);
+        add(lblName);
 
+        // Eingabefeld für Text
         txtName = new JTextField();
-        txtName.setBounds(startX + abstand + breite, startY, 100, hoehe);
+        // Größe und Position in einem
+        txtName.setBounds(startX + abstand + breite, startY, 150, hoehe);
         add(txtName);
 
         // 2. Zeile: Button
+        startY += hoehe + abstand;
+        // Schaltfläche
         btnGruesse = new JButton("Klick mich!");
-        startY += abstand + hoehe;
-        btnGruesse.setBounds(startX + abstand + breite, startY, 100, 20);
-
-        // Callback an das fenster
+        btnGruesse.setBounds(startX + abstand + breite, startY, 100, hoehe);
+        // das Fenster selber als ActionListener-Implementierung registrieren
         btnGruesse.addActionListener(this);
-
         add(btnGruesse);
 
-        // 3. Feld für Message
-        startY += abstand + hoehe;
-        lbMessage = new JLabel("");
-        lbMessage.setSize(breite, 20);
-        lbMessage.setLocation(startX, startY);
-        lbMessage.setBounds(startX, startY, 200, hoehe);
-        lbMessage.setBorder(BorderFactory.createLineBorder(Color.ORANGE));
-        add(lbMessage);
+        // 3. Zeile: Feld für Message
+        startY += hoehe + abstand;
+        lblMessage = new JLabel("");
+        lblMessage.setBounds(startX, startY, 225, hoehe);
+        lblMessage.setBorder(BorderFactory.createLineBorder(Color.ORANGE));
+        add(lblMessage);
 
-        // Callback für das Klick-Ereignis
-/*
-        btnGruesse.addActionListener(e -> {
-            System.out.println("Click auf btngrüße. name = " + txtName.getText());
-            lbMessage.setText("Hallo, " + txtName.getText());
-        });
-*/
-
-        // Standard-Operation  ist Verbergen
-        // stattdessen das Fenster beim Schließen zerstöre
-        // damit wird das Programm bbeendet, wenn dieses Fenster das letzte
+        // Standard-Operation ist Verbergen
+        // stattdessen: das Fenster beim Schließen zerstören
+        // damit wird das Programm beendet, wenn dieses Fenster das letzte
         // Fenster der Anwendung ist
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
     }
+
+
 
     public static void main(String[] args) {
         HelloSwing2 hauptfenster = new HelloSwing2();
+        // das Hauptfenster anzeigen
         hauptfenster.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.println("Click auf btngrüße. name = " + txtName.getText());
-        lbMessage.setText("Hallo, " + txtName.getText());
+        System.out.println("Click auf btnGruesse, name = " + txtName.getText());
+        lblMessage.setText("Hallo, " + txtName.getText());
     }
 }
