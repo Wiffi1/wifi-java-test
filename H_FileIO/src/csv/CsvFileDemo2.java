bd0c3a7df3fed44e398f362564aac816507f14ec
IrnbergerF <irnbergf@gmail.com>
2021-11-11 13:42:11 +0100

    funzt nicht gescheit

--- H_FileIO/src/csv/CsvFileDemo2.java
+++ H_FileIO/src/csv/CsvFileDemo2.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+// Die Werte werden mit eigenen Formatern formatier und geparst
 public class CsvFileDemo2 {
 
 	private static Scanner scanner = new Scanner(System.in);
