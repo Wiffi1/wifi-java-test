24171b8e2f0c7caf6d77668f9b326f54cf516035
LevMan <franz.irnberger@gmail.com>
2021-12-09 12:11:34 +0100

    streamApi new

---
+++ J_StreamApi/src/animalsStream/StreamApiDemo.java
@@ -0,0 +1,16 @@
+package animalsStream;
+
+public class StreamApiDemo {
+    public static void main(String[] args) {
+        Animal[] allAnimals = new Animal[] {
+            new Animal("Löwe", "Panthera leo", 250, false),
+            new Animal("Waschbär", "Procyon", 12, false),
+            new Animal("Bison", "Bison bison", 950, true),
+            new Animal("Elefant", "Loxodonta", 4000, true),
+            new Animal("Eisbär", "Ursus maritimus", 650, false),
+            new Animal("Emu", "Dromaius novaehollandiae", 37, false)
+
+
+        };
+    }
+}
