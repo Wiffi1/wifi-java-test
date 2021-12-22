9cfaacdb66342db5c2c4ee66d978b92e639b859d
LevMan <franz.irnberger@gmail.com>
2021-12-22 16:05:24 +0100

    Socket

--- N_Network/src/echo/EchoClient.java
+++ N_Network/src/echo/EchoClient.java
@@ -14,14 +14,30 @@ public class EchoClient {
 		String hostName = "localhost";
 		
 		try {
-		
+			// Verbindung zum Server herstellen (Port muss mit Server übereinstimmen.
+			Socket clientSocket = new Socket(hostName, 4321);
+
+			// Reader und Writer erzeugen, die über der Socket lesen/schreiben
+			BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), "UTF-8"));
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream(), "UTF-8"));
+
 			System.out.println("Daten zeilenweise eingeben, beenden mit Leerzeile");
 			String line;
 			// solang der User Text eingeben hat
 			while ((line = scanner.nextLine()) != null && !line.isEmpty()) {
 				System.out.println("Sende Text: " + line);
+				// Daten senden
+				writer.write(line);
+				// newline senden
+				writer.newLine();
+				// senden erzwingen
+				writer.flush();
+				// die Antwort lesen
+				String response = reader.readLine();
+				System.out.println("Antwort vom Server: " + response);
 			}
-			
+			clientSocket.close();
+
 		} catch (Exception e) {
 			System.err.println("Fehler im EchoClient:");
 			e.printStackTrace();
--- N_Network/src/echo/EchoServer.java
+++ N_Network/src/echo/EchoServer.java
@@ -15,19 +15,41 @@ public class EchoServer {
 		System.out.println("Server wird gestartet");
 
 		try {
+			ServerSocket server = new ServerSocket(4321);
 			boolean keepRunning = true;
 			while (keepRunning) {
 				System.out.println("Warte auf Clients...");
-				// TODO: Client-Verbindung annehmen
+				// Client-Verbindung annehmen (blockiert, bis sich ein Client gefunden hat)
+				Socket client = server.accept();
+				// Info zum Client anzeigen
+				System.out.println("Client hat sich verbunden: " + client.getInetAddress());
 				try {
-					// TODO: Daten mit dem Client austauschen
+					// Reader und Writer
+					// Daten mit dem Client austauschen
+					BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream(), "UTF-8"));
+					BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream(), "UTF-8"));
+					// Daten mit dem Client austauschen (Reihenfolge muss mit dem Client zusammenpassen)
+					String line;
+					// So lange der Client Daten sendet (readline liefert null, wenn EOF erreicht ist;
+					// EOF wird vom Reader gesendet, wenn der Client die Verbindung schließt)
+					while ((line = reader.readLine()) != null) {
+						System.out.println("Vom Client empfangen: " + line);
+						// die Daten zurückschicken
+						writer.write("echo: " + line);
+						writer.newLine();
+						// Das Senden der Daten jetzt erzwingen
+						writer.flush();
+					}
 				} catch (Exception e) {
 					System.err.println("Fehler in Client-Kommunikation");
 					e.printStackTrace();
 				}
 				System.out.println("Der Client hat keine weiteren Daten gesendet");
-				
+				// alle Ressourcen für den Socket freigeben
+				client.close();
 			}
+			// wenn die Serverschleife zu Ende ist, auch die serverseitigen Ressourcen freigeben.
+			server.close();
 		} catch (Exception e) {
 			System.err.println("Fehler im EchoServer:");
 			e.printStackTrace();
